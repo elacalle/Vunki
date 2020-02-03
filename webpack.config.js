d860b3f2190035a6da31d5f7fc7644aaa143cad8
elacalle <ernestohlacalle@gmail.com>
2020-02-04 00:58:56 +0100

    CSS generation in a split file
    Configured a Webpack plugin to compile a CSS file in a separate file

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    output: {
-        filename: '[hash].js'
-    },
-    module: {
-      rules: [
-        {test: /\.css$/, use: ['style-loader', 'css-loader']},
-        {test: /\.html$/, use: 'html-loader'}
-      ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Vunki',
-            template: 'template.html'
-        }),
-        new webpack.ProvidePlugin({
-          Vue: ['vue/dist/vue.esm.js', 'default'],
-          Dexie: ['dexie', 'default'],
-          Database: ['db', 'default']
-        }),
-        new CleanWebpackPlugin()
-    ],
-    resolve: {
-      alias: {
-        db: path.resolve(__dirname, './src/services/database/config.js')
+  output: {
+    filename: "[hash].js"
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+      { test: /\.html$/, use: "html-loader" }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Vunki",
+      template: "template.html"
+    }),
+    new webpack.ProvidePlugin({
+      Vue: ["vue/dist/vue.esm.js", "default"],
+      Dexie: ["dexie", "default"],
+      Database: ["db", "default"]
+    }),
+    new CleanWebpackPlugin()
+  ],
+  resolve: {
+    alias: {
+      db: path.resolve(__dirname, "./src/services/database/config.js")
     }
   }
-}
+};
