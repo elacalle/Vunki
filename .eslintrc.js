29f4f81470e1867ad9c78d907af5f6e3c0c54985
elacalle <ernestohlacalle@gmail.com>
2020-02-12 10:48:13 +0100

    Fixed cypress jslint global variables error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,19 @@
 module.exports = { 
+  extends: "standard",
   globals: {
     Vue: 'writable',
     Dexie: 'writable',
     Database: 'writable'
   },
-  "extends": "standard"
+  plugins: [
+    "cypress"
+  ],
+  rules: {
+    "cypress/no-assigning-return-values": "error",
+    "cypress/no-unnecessary-waiting": "error",
+    "cypress/assertion-before-screenshot": "warn"
+  },
+  env: {
+    "cypress/globals": true
+  }
 };
